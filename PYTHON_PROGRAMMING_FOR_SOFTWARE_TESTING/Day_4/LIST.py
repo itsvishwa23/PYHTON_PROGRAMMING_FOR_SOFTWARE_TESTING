a2d7dd45c54835ca4b4a5b2bcdb1879e06cff645
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2024-08-11 18:01:29 +0530

    Day_1

--- PYTHON_PROGRAMMING_FOR_SOFTWARE_TESTING/Day_4/LIST.py
+++ PYTHON_PROGRAMMING_FOR_SOFTWARE_TESTING/Day_4/LIST.py
@@ -1,5 +1,34 @@
-# list is in orrder and we can change
-lst = [1, 2, 3]
+# list is in order and we can change
+
+lst = [1, "Banana", 3]
+lst2 = [11, 12, 13]
 for i in lst:
     print(i)
 print(len(lst))
+
+print(len(lst2))
+print(lst[0:3])
+
+lst.append("Orange")
+for i in lst:
+    print(i)
+print(len(lst))
+
+lst.insert(2, "Pineapple")
+for i in lst:
+    print(i)
+print(len(lst))
+
+lst.reverse()
+for i in lst:
+    print(i)
+
+lst3 = lst + lst2
+
+for j in lst3:
+    print(j)
+
+sst = {10, 10}
+print(len(sst))
+
+https://youtu.be/Q0ESmzic20c?t=3177
