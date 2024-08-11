1c19e596f5f45cd2a32ef21be900c7d9d41fd132
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2024-08-11 12:23:31 +0530

    Day_1

---
+++ PYTHON_PROGRAMMING_FOR_SOFTWARE_TESTING/Day_1/COMMENTS.py
@@ -0,0 +1,11 @@
+# Hello World Demo
+#
+# this is python
+# this is python
+# this is python
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
---
+++ PYTHON_PROGRAMMING_FOR_SOFTWARE_TESTING/Day_1/VARIBLES.py
@@ -0,0 +1,15 @@
+a = 10
+b = 25.5
+c = True
+d = "Vishwa"
+
+# a = "Test"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(type(a))
+result = a + b
+print(result)
