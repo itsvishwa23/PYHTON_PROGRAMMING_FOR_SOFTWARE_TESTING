8b3a49bc6c28e69f22179609b434efd6cdd3a59b
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2024-08-16 11:38:16 +0530

    :100:

---
+++ PYTHON_PROGRAMMING_FOR_SOFTWARE_TESTING/MY_PRACTISE/MY_PRACTISE.py
@@ -0,0 +1,15 @@
+try:
+    qty = [10, 10, 20, 30]
+except:
+    for i in qty:
+        print(i)
+else:
+    print()
+finally:
+    print()
+sst = set(qty)
+print(sst)
+
+name = "Vishwanath"
+sst = set(name)
+print(name)
